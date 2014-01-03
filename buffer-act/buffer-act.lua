a70c199d1c696e7d3c44d9bf14939017bc745062
Symmetryc <yuiopqwertyasdfglkjh@hotmail.com>
2014-01-03 18:38:55 -0500

    Update buffer-act.lua

--- buffer-act/buffer-act.lua
+++ buffer-act/buffer-act.lua
@@ -1,67 +1,74 @@
 -- Buffer-Act, By Symmetryc
+local pairs, twrite, ttext, tback, tpos, tclear, tblink = pairs, term.write, term.setTextColor, term.setBackgroundColor, term.setCursorPos, term.clear, term.setCursorBlink
 return {
-        new = function()
-                return {
-                        act = {};
-                        pos_x = 1;
-                        pos_y = 1;
-                        shift_x = 0;
-                        shift_y = 0;
-                        back = colors.white;
-                        text = colors.black;
-                        blink = false;
-                        write = function(self, _str)
-                                local act = self.act
-                                local append = true
-                                if self.pos_x ~= act.pos_y or self.pos_x ~= act.pos_y then
-                                        act[#act + 1] = {term.setCursorPos, self.pos_x, self.pos_y}
-                                        append = false
-                                end
-                                if self.back ~= act.back then
-                                        act[#act + 1] = {term.setBackgroundColor, self.back}
-                                        act.back = self.back
-                                        append = false
-                                end
-                                if self.text ~= act.text then
-                                        act[#act + 1] = {term.setTextColor, self.text}
-                                        act.text = self.text
-                                        append = false
-                                end
-                                for line, nl in _str:gmatch("([^\n]*)(\n?)") do
-                                        if append then
-                                                act[#act][2] = act[#act][2]..line
-                                                append = false
-                                        else
-                                                act[#act + 1] = {term.write, line}
-                                        end
-                                        if nl == "\n" then
-                                                self.pos_y = self.pos_y + 1
-                                                act[#act + 1] = {term.setCursorPos, self.pos_x, self.pos_y}
-                                        else
-                                                self.pos_x = self.pos_x + #line
-                                        end
-                                end
-                                act.pos_x, act.pos_y = self.pos_x, self.pox_y
-                                return self
-                        end;
-                        clear = function(self)
-                                self.act = {
-                                        {term.setBackgroundColor, self.back};
-                                        {term.clear};
-                                }
-                                return self
-                        end;
-                        draw = function(self)
-                                for i, v in ipairs(self.act) do
-                                        if v[3] then
-                                                v[1](v[2] + self.shift_x, v[3] + self.shift_y)
-                                        else
-                                                v[1](v[2])
-                                        end
-                                end
-                                term.setCursorBlink(self.blink)
-                                return self
-                        end;
-                }
-        end;
+	new = function()
+		local act, pos_x_, pos_y_, back_, text_ = {}
+		return {
+			pos_x = 1, pos_y = 1;
+			back = colors.black, text = colors.white;
+			shift_x = 0, shift_y = 0;
+			blink = false; -- CHECK TO SEE IF term.setCursorBlink(nil) WILL ALSO WORK, IN WHICH CASE, NO NEED TO DEFINE THIS
+			-- CHECK
+			-- CHECK
+			-- REMINDER TO CHECK
+			--   CZECH
+			--   CZECH
+			--   CZECH
+			-- MY HANDS ARE TYPING WORDS
+			-- CHECK
+			-- CHECK
+			write = function(self, _str)
+				local append, pos_x, pos_y, back, text = true, self.pos_x, self.pos_y, self.back, self.text
+				if pos_x ~= pos_x_ or pos_y ~= pos_y_ then
+                    act[#act + 1] = {tpos, pos_x, pos_y}
+					append = false
+				end
+				if back ~= back_ then
+					act[#act + 1] = {tback, back}
+					back_ = back
+					append = false
+				end
+				if text ~= text_ then
+					act[#act + 1] = {ttext, text}
+					text_ = text
+					append = false
+				end
+				for line, nl in _str:gmatch("([^\n]*)(\n?)") do
+					if append then
+						act[#act][2] = act[#act][2]..line
+						append = false
+					else
+						act[#act + 1] = {twrite, line}
+					end
+					if nl == "\n" then
+						pos_y = pos_y + 1
+						act[#act + 1] = {tpos, pos_x, pos_y}
+					else
+						pos_x = pos_x + #line
+					end
+				end
+				pos_x_, pos_y_, self.pos_x_, self.pos_y_ = pos_x, pos_y, pos_x, pos_y
+				return self
+			end;
+			clear = function(self)
+				pos_x_, pos_y_, back_, text_, act = nil, nil, self.back, nil, {
+					{tback, self.back};
+					{tclear};
+				}
+				return self
+			end;
+			draw = function(self)
+				local shift_x, shift_y = self.shift_x, self.shift_y
+				for k, v in pairs(act) do
+					if v[3] then
+						v[1](v[2] + shift_x, v[3] + shift_y)
+					else
+						v[1](v[2])
+					end
+				end
+				tblink(self.blink)
+				return self
+			end;
+		}
+	end;
 }
