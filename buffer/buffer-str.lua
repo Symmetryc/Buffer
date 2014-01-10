74ea99bf92e1274ce0ddc705f9ba18d77022e06a
Symmetryc <yuiopqwertyasdfglkjh@hotmail.com>
2014-01-09 19:49:55 -0500

    Update

--- buffer/buffer-str.lua
+++ buffer/buffer-str.lua
@@ -1,17 +1,18 @@
 -- Buffer-Str, By Symmetryc
 return {
-	new = function()
-		local str_, text_, back_, draw = "", "", ""
+	new = function(_w, _h)
+		local str_, text_, back_ = "", "", ""
 		return {
 			pos_x = 1, pos_y = 1;
 			back = 32768, text = 1;
+			shift_x = 0, shift_y = 0;
 			blink = false;
 			clear = function(self)
 				str_, text_, back_ = "", "", ""
 				return self
 			end;
 			write = function(self, _str)
-				
+
 			end;
 			render = function(self)
 				--#stuff
